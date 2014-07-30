a4cfa8c57f3a64491ffdb8b6f27777d4a4f0c9e1
SatrioDwiPrabowo <sdprabowo77@gmail.com>
2014-07-31 02:28:28 +0700

    Intuisy Kernel source

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Tue Jul 29 16:56:10 WIB 2014"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "Society-Ubuntu"
+#define LINUX_COMPILER "gcc version 4.7.3 20130102 (prerelease) (Linaro GCC 4.7-2013.01) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "3.4.40-XSSDP-Intuisy-Inside-7.0"
